37d0cc7ad51cb17faec5ced7193e5f0013cd51d4
unknown <tamas.vajk@sonarsource.com>
2015-04-10 10:41:53 +0200

    Code Quality Fixes

--- SonarQube.CSharp/SonarQube.Runner/DiagnosticsRunner.cs
+++ SonarQube.CSharp/SonarQube.Runner/DiagnosticsRunner.cs
@@ -9,19 +9,19 @@ namespace SonarQube.Runner
     public class DiagnosticsRunner
     {
         private readonly ImmutableArray<DiagnosticAnalyzer> diagnosticAnalyzers;
-        
+
         public DiagnosticsRunner(ImmutableArray<DiagnosticAnalyzer> diagnosticAnalyzers)
         {
-			foreach (var analyzer in diagnosticAnalyzers)
-			{
-				foreach (var diagnostic in analyzer.SupportedDiagnostics)
-				{
-					diagnostic.GetType().GetProperty("IsEnabledByDefault").SetValue(diagnostic, true);
-				}	
-			}
+            foreach (var analyzer in diagnosticAnalyzers)
+            {
+                foreach (var diagnostic in analyzer.SupportedDiagnostics)
+                {
+                    diagnostic.GetType().GetProperty("IsEnabledByDefault").SetValue(diagnostic, true);
+                }
+            }
 
-			this.diagnosticAnalyzers = diagnosticAnalyzers;
-		}
+            this.diagnosticAnalyzers = diagnosticAnalyzers;
+        }
 
         public IEnumerable<Diagnostic> GetDiagnostics(Compilation compilation)
         {
